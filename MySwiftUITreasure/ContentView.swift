7493e5df4d2d74144fe244996f64075dd7dcf0dd
mengjiaoShortcut <mengjiao.yang@shortcut.no>
2021-09-21 15:07:52 -0500

    Initial Commit

---
+++ MySwiftUITreasure/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MySwiftUITreasure
+//
+//  Created by mengjiao on 9/21/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MySwiftUITreasure/MySwiftUITreasureApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftUITreasureApp.swift
+//  MySwiftUITreasure
+//
+//  Created by mengjiao on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftUITreasureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
